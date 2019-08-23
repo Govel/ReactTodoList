bd0f9306f115dca89516f3946ba8072047c918e3
MeeCKY <s.gorlov123@gmail.com>
2019-08-23 21:39:28 +0300

    Updates

---
+++ precache-manifest.0edb14416de0c82788733d7c9f0c86ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bc68ce1faa6743f1d27263916c113a",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "294de001c1b3ace536c0",
+    "url": "/ReactTodoList/static/css/main.dc90d239.chunk.css"
+  },
+  {
+    "revision": "2fc382a08374bd23f53e",
+    "url": "/ReactTodoList/static/js/2.631a7925.chunk.js"
+  },
+  {
+    "revision": "294de001c1b3ace536c0",
+    "url": "/ReactTodoList/static/js/main.fe1b012b.chunk.js"
+  },
+  {
+    "revision": "8c84ea70b1d69b55fbd1",
+    "url": "/ReactTodoList/static/js/runtime~main.2f299d9a.js"
+  }
+]);
