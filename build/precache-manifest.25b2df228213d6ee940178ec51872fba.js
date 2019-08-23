79407fd8fff17f87383deac6673494f18ad5b458
MeeCKY <s.gorlov123@gmail.com>
2019-08-23 13:10:08 +0300

    Release 1.0.0

---
+++ build/precache-manifest.25b2df228213d6ee940178ec51872fba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc0539846873e15fa19f058ebedad31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "294de001c1b3ace536c0",
+    "url": "/static/css/main.dc90d239.chunk.css"
+  },
+  {
+    "revision": "2fc382a08374bd23f53e",
+    "url": "/static/js/2.631a7925.chunk.js"
+  },
+  {
+    "revision": "294de001c1b3ace536c0",
+    "url": "/static/js/main.fe1b012b.chunk.js"
+  },
+  {
+    "revision": "b880043bbae41cca61d7",
+    "url": "/static/js/runtime~main.aa556c89.js"
+  }
+]);
